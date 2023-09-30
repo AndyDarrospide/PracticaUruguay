3d72ab0fa61eec62e775d19d2a1e178fa24e869b
AndyDarrospide <soledadmigala@gmail.com>
2023-09-30 13:48:11 -0300

    Se crea proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*=============== SHOW MENU ===============*/
+const MENU = document.getElementById ('nav-menu')
+const toggleButton = document.getElementById ('nav-toggle')
+const close Button = document.getElementById ('nav-close')
+
+(toggleButton) {
+    toggleButton.addEventListener ('click', () => {
+        menubar.classList.add('show-menu')
+    })
+     
+}
+
+if (closeButton) {
+    closeButton.addEventListener('click', () => {
+        menu.classList.remove('show-menu')
+    })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== GSAP ANIMATION ===============*/
+
